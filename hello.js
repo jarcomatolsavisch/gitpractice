64d94c7a7b3fb9638ace61f283736a1bd8cee6cd
James Ma <ma6036tp@gmail.com>
2021-07-02 11:30:23 +0800

    修改hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,12 @@
-console.log("Hello World")
-console.log("Good Bye~")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8888;
+app.get("/",(req,res)=>{
+    res.end("Hello World.");
+});
+app.get("/test",(req,res)=>{
+    res.end("Testing");
+});
+app.listen(port,()=>{
+    console.log(`Server listens on port =${port}`);
+});
